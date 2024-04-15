848d8f0a0130233253211d498774055f3db78538
pythog <141962883+pyth0g@users.noreply.github.com>
2024-04-15 19:42:47 +0200

    Delete Python/Args directory

--- Python/Args/print-no_switch.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('print', help='Print a message')
-args = parser.parse_args()
-if args.print:
-    print(args.print)
--- Python/Args/print-switch.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--print', dest='message', help='Print a message')
-args = parser.parse_args()
-if args.message:
-    print(args.message)
