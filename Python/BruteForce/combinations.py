e6133ea06abae0e2a4907d4fc8562692152ba78f
pythog <141962883+pyth0g@users.noreply.github.com>
2024-04-15 19:44:02 +0200

    Delete Python/BruteForce directory

--- Python/BruteForce/combinations.py
+++
@@ -1,8 +0,0 @@
-from itertools import product
-
-def _calc(symbols,len):
-    yield from product(*([symbols] * len))
-
-def generate(symbols,len):
-    for x in _calc(symbols,len):
-        yield (''.join(x))
