58554865438ad893c6003c6d00bce38c9b537cd2
pythog <141962883+pyth0g@users.noreply.github.com>
2024-04-16 13:57:19 +0200

    Delete Python/BfScript/Code/testing.py

--- Python/BfScript/Code/testing.py
+++
@@ -1,9 +0,0 @@
-import bfscript
-
-file_path = r"C:\Users\lenar\OneDrive\PYthon\BfScript\test.bfs"
-
-with open(file_path) as f: code = f.read()
-
-compiled = bfscript.Compiler.compile(code)
-
-print(compiled)
